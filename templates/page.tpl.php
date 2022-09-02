a69629a9c78949be18d1ec9eaeb9f42a623aa6d0
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-09-02 10:15:44 -0700

    Add css so that the documents table looks the same as it does in the file service module.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -9,3 +9,22 @@
 
 <?php print $documents; ?>
 
+
+<style>
+.table-row p.table-cell,
+.table-row p.table-header {
+    padding:7px;
+    padding-left:13px;
+    padding-right:13px;
+}
+
+
+.icon-cell {
+    text-align: center;
+}
+
+.doc-list {
+    margin: 0 0 4% 0;
+}
+</style>
+
